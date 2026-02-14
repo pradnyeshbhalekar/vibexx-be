560cb347cc307a51ef440de65c1cd705173e75d9
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-02-14 12:33:01 +0530

    spotfiy. client

--- app/services/spotify_service.py
+++ app/services/spotify_service.py
@@ -1,21 +1,12 @@
 import spotipy
-from flask import session,jsonify
 from app.utils.spotify_auth import get_spotify_oauth
 
 
 def get_spotify_client():
-    token_info = session.get("spotify_token")
+    sp_oauth = get_spotify_oauth()
 
+    token_info = sp_oauth.get_cached_token()
     if not token_info:
         raise Exception("User not authenticated with Spotify")
-    
-
-    sp_oauth = get_spotify_oauth()
-    if not sp_oauth:
-        return jsonify({"error":"not authenticated"}),401
-
-    if sp_oauth.is_token_expired(token_info):
-        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
-        session["spotify_token"] = token_info
 
     return spotipy.Spotify(auth=token_info["access_token"])
